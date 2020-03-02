fc6058d7ef4f2f234c81df3299586990913dc90c
Marcin Puc <marcin.e.puc@gmail.com>
2020-03-02 08:35:55 +0100

    Add solution for day 5

---
+++ src/days/d05p1.rs
@@ -0,0 +1,32 @@
+use std::io::{BufRead, Error, ErrorKind};
+
+use aoc::intcode::Machine;
+use aoc::utils::BufferedInput;
+
+fn parse_input() -> std::io::Result<Vec<i64>> {
+    let input = BufferedInput::parse_args("Day 5: Sunny with a Chance of Asteroids - Part 1")?;
+    let line = input.lines()
+        .map(Result::unwrap)
+        .next()
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Input has no content"))?;
+
+    let result = line.as_str()
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    Ok(result)
+}
+
+fn main() -> std::io::Result<()> {
+    let program = parse_input()?;
+
+    let mut machine = Machine::new(program, || 1, |_| ());
+    machine.run();
+
+    let code = machine.last_output().unwrap();
+
+    println!("{}", code);
+
+    Ok(())
+}
