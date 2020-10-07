3f5db9fb1ef335edd698ad0e05292cf716540828
Marcin Puc <marcin.e.puc@gmail.com>
2020-10-07 18:02:26 +0200

    Simplify example tests

--- tests/example_test.rs
+++ tests/example_test.rs
@@ -1,18 +1,15 @@
 use assert_cmd::Command;
-use predicates::prelude::*;
 
 #[test]
 fn day_one_part_one() {
     let input = "12\n14\n1969\n100756";
     let expected = 2 + 2 + 654 + 33583;
+    let expected_str = format!("{}\n", expected);
 
     let mut command = Command::cargo_bin("d01p1").unwrap();
     let assert = command.write_stdin(input).assert();
 
-    assert.success().stdout(predicate::function(|out| {
-        let output: i32 = String::from_utf8_lossy(out).trim().parse().unwrap();
-        output == expected
-    }));
+    assert.success().stdout(expected_str);
 }
 
 #[test]
@@ -20,26 +17,21 @@ fn day_one_part_two() {
     let input = "12\n14\n1969\n100756";
     let expected =
         2 + 2 + (654 + 216 + 70 + 21 + 5) + (33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2);
+    let expected_str = format!("{}\n", expected);
 
     let mut command = Command::cargo_bin("d01p2").unwrap();
     let assert = command.write_stdin(input).assert();
 
-    assert.success().stdout(predicate::function(|out| {
-        let output: i32 = String::from_utf8_lossy(out).trim().parse().unwrap();
-        output == expected
-    }));
+    assert.success().stdout(expected_str);
 }
 
 #[test]
 fn day_two_part_one() {
     let input = "1,1,1,4,99,5,6,0,99";
-    let expected = 30;
+    let expected_str = "30\n";
 
     let mut command = Command::cargo_bin("d02p1").unwrap();
     let assert = command.write_stdin(input).assert();
 
-    assert.success().stdout(predicate::function(|out| {
-        let output: i32 = String::from_utf8_lossy(out).trim().parse().unwrap();
-        output == expected
-    }));
+    assert.success().stdout(expected_str);
 }
