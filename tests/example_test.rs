d98d193679e21567215f478a4c0a037a53fc9b46
Marcin Puc <marcin.e.puc@gmail.com>
2020-10-07 17:42:06 +0200

    Add example tests for day 1

---
+++ tests/example_test.rs
@@ -0,0 +1,31 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn day_one_part_one() {
+    let input = "12\n14\n1969\n100756";
+    let expected = 2 + 2 + 654 + 33583;
+
+    let mut command = Command::cargo_bin("d01p1").unwrap();
+    let assert = command.write_stdin(input).assert();
+
+    assert.success().stdout(predicate::function(|out| {
+        let output: i32 = String::from_utf8_lossy(out).trim().parse().unwrap();
+        output == expected
+    }));
+}
+
+#[test]
+fn day_one_part_two() {
+    let input = "12\n14\n1969\n100756";
+    let expected =
+        2 + 2 + (654 + 216 + 70 + 21 + 5) + (33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2);
+
+    let mut command = Command::cargo_bin("d01p2").unwrap();
+    let assert = command.write_stdin(input).assert();
+
+    assert.success().stdout(predicate::function(|out| {
+        let output: i32 = String::from_utf8_lossy(out).trim().parse().unwrap();
+        output == expected
+    }));
+}
